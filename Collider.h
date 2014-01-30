fdb92ae89e201b7a35006d7fe932dfc830220c5e
Carveca <carveca2@gmail.com>
2014-01-30 07:25:44 +0100

    1

---
+++ Collider.h
@@ -0,0 +1,29 @@
+//Collider header file
+
+#pragma once
+
+#include <SFML/System/Vector2.hpp>
+
+class InteractiveObject;
+
+class Collider
+{
+public:
+	Collider();
+	~Collider();
+
+	bool OverlapBoxVsBox(InteractiveObject* other);
+	bool OverlapCircleVsCircle(InteractiveObject* other);
+			
+	sf::Vector2f GetPosition();
+	void SetPosition(sf::Vector2f position);
+
+	sf::Vector2f GetExtension();
+	void SetExtension(sf::Vector2f extension);
+	
+
+protected:
+	sf::Vector2f m_position;
+	sf::Vector2f m_extension;
+
+};
