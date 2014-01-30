c7d3bfd2f14cf1ae9a1b47f3578878e7f4489dac
Carveca <carveca2@gmail.com>
2014-01-30 06:54:59 +0100

    Main c++

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//main.cpp
+
+#include "Player.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	Player player;
+
+	std::string name = "Lolol";
+
+	player.SetName(name);
+
+	std::cout << player.GetName() << std::endl;
+
+	std::cout << player.GetPosition().y << std::endl;
+
+	player.MoveUp();
+
+	std::cout << player.GetPosition().y << std::endl;
+	
+	system("pause");
+
+	return 0;
+}
