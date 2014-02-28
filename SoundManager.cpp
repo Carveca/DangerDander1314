741863815bdaded305b78ba00d9e4ded40758ff3
GusThoEng <gustafte@gmail.com>
2014-02-28 10:36:58 +0100

    Sound(it works!)

--- SoundManager.cpp
+++ SoundManager.cpp
@@ -11,7 +11,7 @@ SoundManager::SoundManager()
 
 SoundManager::~SoundManager()
 {
-	m_buffer.clear();
+
 }
 
 bool SoundManager::Initialize(const sf::String directory)
@@ -21,22 +21,29 @@ bool SoundManager::Initialize(const sf::String directory)
 	return true;
 }
 
+void SoundManager::Cleanup()
+{
+	m_buffer.clear();
+}
+
 void SoundManager::LoadSound(std::string file_name, std::string sound_name)
 {
+	std::string name = sound_name;
 	std::string buffername = sound_name + "Buffer";
 	sf::SoundBuffer buffer;
+	sf::Sound sound;
 
 	if(!buffer.loadFromFile( m_pathname + file_name))
 	{
 		std::cout<<"Error, could not load sound file"<<std::endl;
 	}
-	
-	m_buffer.insert(std::pair <std::string, sf::SoundBuffer>(buffername, buffer));
 
+	m_buffer.insert(std::pair <std::string, sf::SoundBuffer>(buffername, buffer));
+	m_sounds.insert(std::pair <std::string, sf::Sound>(name, sound));
 }
 
 void SoundManager::PlaySound(std::string name)
 {
-	m_sound.setBuffer(m_buffer[name]);
-	m_sound.play();
+	m_sounds[name].setBuffer(m_buffer[name + "Buffer"]);
+	m_sounds[name].play();
 }
--- SoundManager.h
+++ SoundManager.h
@@ -12,12 +12,13 @@ public:
 	~SoundManager();
 
 	bool Initialize(const sf::String directory);
+	void Cleanup();
 	void LoadSound(std::string file_name, std::string sound_name);
 	void PlaySound(std::string name);
 
 private:
 	std::string m_pathname;
 
-	sf::Sound m_sound;
+	std::map<std::string, sf::Sound> m_sounds;
 	std::map<std::string, sf::SoundBuffer> m_buffer; 
 };
