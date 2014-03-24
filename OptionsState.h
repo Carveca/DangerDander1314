c3f557d0dd3e0d50121eb67152b4c2a670939b32
GusThoEng <gustafte@gmail.com>
2014-03-24 10:54:55 +0100

    File writer and early options state

---
+++ OptionsState.h
@@ -0,0 +1,40 @@
+//OptionsState.h
+
+#pragma once
+
+#include "StateII.h"
+
+class SpriteManager;
+class MusicManager;
+class Sprite;
+
+class OptionsState : public StateII
+{
+public:
+	OptionsState();
+
+	bool Enter(SpriteManager* spritemanager, MusicManager* musicmanager);
+	void Exit();
+
+	bool Update(float &deltatime);
+	void Draw(sf::RenderWindow* window);
+	void Draw(sf::RenderWindow* window, float &deltatime);
+
+	std::string Next();
+	bool IsType(const std::string &type);
+
+protected:
+	SpriteManager* m_spriteManager;
+	MusicManager* m_musicManager;
+	FileWriter* m_writer;
+	FileReader* m_reader;
+
+	sf::Sprite* m_background;
+
+	int m_musicVolume;
+	int m_soundVolume;
+	float m_keytimer;
+	int m_selection;
+	bool m_done;
+	std::string m_nextState;
+};
